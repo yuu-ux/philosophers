4282444dec4f11693daba8956b80fb0f593f15f7
yuu-ux <yuugaehara@gmail.com>
2024-11-02 23:16:19 +0900

    :construction: デッドロック解消中

--- src/create_thread.c
+++ src/create_thread.c
@@ -1,36 +1,4 @@
 #include "../include/h_philo.h"
-#include <pthread.h>
-
-
-int	philosopher_dead(t_philo *philo, size_t time_to_die)
-{
-	pthread_mutex_lock(philo->meal_mtx);
-	// philo->eatingもandで条件入れてる, 何かわからない
-	if (get_current_time() - philo->last_time >= time_to_die)
-		return (pthread_mutex_unlock(philo->meal_mtx), 1);
-	pthread_mutex_unlock(philo->meal_mtx);
-	return (0);
-}
-
-int	check_if_dead(t_philo *philos)
-{
-	int	i;
-
-	i = 0;
-	while (i < philos[0].num_of_philos)
-	{
-		if (philosopher_dead(&philos[i], philos[i].time_to_die))
-		{
-			print_message(&philos[i], "died");
-			pthread_mutex_lock(philos[0].death_mtx);
-			philos->is_dead = 1;
-			pthread_mutex_unlock(philos[0].death_mtx);
-			return (1);
-		}
-		i++;
-	}
-	return (0);
-}
 
 int	check_if_all_ate(t_philo *philos)
 {
@@ -59,14 +27,44 @@ int	check_if_all_ate(t_philo *philos)
 	return (0);
 }
 
+int	philosopher_dead(t_philo *philo, size_t time_to_die)
+{
+	pthread_mutex_lock(philo->write_mtx);
+	if ((get_current_time() - philo->last_time) >= time_to_die)
+		return (pthread_mutex_unlock(philo->meal_mtx), 1);
+	pthread_mutex_unlock(philo->write_mtx);
+	return (0);
+}
+
+int	check_if_dead(t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < philos[0].num_of_philos)
+	{
+		if (philosopher_dead(&philos[i], philos[i].time_to_die))
+		{
+			print_message(&philos[i], "died");
+			pthread_mutex_lock(philos[0].death_mtx);
+			philos->is_dead = 1;
+			pthread_mutex_unlock(philos[0].death_mtx);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
+}
+
 void	*monitor(void *_philos)
 {
     t_philo *philos;
 
     philos = (t_philo *)_philos;
+
     while (1)
     {
-		if (check_if_dead(philos) == 1 || check_if_all_ate(philos) == 1)
+		if (check_if_dead(philos) == 1) // || check_if_all_ate(philos) == 1
 			break ;
     }
     return (philos);
@@ -82,6 +80,10 @@ void    *action_philo(void *_philo)
         eating(philo);
         sleeping(philo);
         thinking(philo);
+		pthread_mutex_lock(philo->death_mtx);
+		if (philo->is_dead)
+			break ;
+		pthread_mutex_unlock(philo->death_mtx);
     }
     return NULL;
 }
@@ -92,9 +94,9 @@ int    create_thread(t_program program, t_philo *philos)
     pthread_t _monitor;
 
     i = 0;
-	_monitor = (pthread_t)malloc(sizeof(pthread_t));
-	if (!_monitor)
-		return (write(1, "Malloc Error\n", 13), false);
+	(void)program;
+    if (pthread_create(&_monitor, NULL, monitor, philos) != 0)
+		return (write(2, "Pthread_create Error\n", 21), false);
     while (i < program.num_of_philos)
     {
 		ft_usleep(300);
@@ -107,14 +109,14 @@ int    create_thread(t_program program, t_philo *philos)
         i++;
     }
 
-    if (pthread_create(&_monitor, NULL, monitor, philos) != 0)
-        return (write(2, "Pthread_create Error\n", 21), false);
-    i = 0;
-    while (i > 0)
+	if (pthread_join(_monitor, NULL) != 0)
+		return (1);
+	i = 0;
+    while (i < program.num_of_philos)
     {
         if (pthread_join(philos[i].thread, NULL) != 0)
             return (1);
-        i--;
+        i++;
     }
     return (0);
 }
